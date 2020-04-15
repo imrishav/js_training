556c5947abe67208166789c5438b0f8bdca49f14
Rishav Sinha <rishav@live.co.uk>
2020-04-15 15:13:43 +0530

    Array Chucking

--- Q-MaxChar.js
+++ Q-MaxChar.js
@@ -6,26 +6,26 @@
  */
 
 const maxChars = str => {
-  const map = {}
-  let max = 0
-  let maxChar = ''
+  const map = {};
+  let max = 0;
+  let maxChar = "";
 
   for (let char of str) {
     if (map[char]) {
-      map[char]++
+      map[char]++;
     } else {
-      map[char] = 1
+      map[char] = 1;
     }
   }
 
   for (chars in map) {
     if (map[chars] > max) {
-      max = map[chars]
-      maxChar = chars
+      max = map[chars];
+      maxChar = chars;
     }
   }
 
-  return `Max Character is ${maxChar} with ${max} occurances`
-}
+  return `Max Character is ${maxChar} with ${max} occurances`;
+};
 
-console.log(maxChars('RRiiisshav'))
+console.log(maxChars("RRiiisshav"));
