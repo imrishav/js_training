c67261790758e078775d5e2e589068a49b96b6f0
Rishav Sinha <rishav@live.co.uk>
2020-06-07 21:28:29 +0530

    reverse List

--- LinkList/linklist.gri.second.js
+++ LinkList/linklist.gri.second.js
@@ -127,7 +127,21 @@ class LinkList {
 
   //Reverse
 
-  reverse() {}
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
 
 let list = new LinkList();
