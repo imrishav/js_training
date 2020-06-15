a3dc03f5493c96828b06591d4fb944e0c954770f
Rishav Sinha <rishav@live.co.uk>
2020-06-16 01:36:10 +0530

    Stack POP & PUSH

--- stacks/stack1.js
+++ stacks/stack1.js
@@ -1,3 +1,43 @@
 // STACKS
 
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null
+     }
+}
  
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        } 
+
+        return ++this.size;
+    }
+
+    pop() {
+
+        if (!this.first) return null;
+        let firstNode = this.first;
+        if (this.size === 1) {
+            this.first = null;
+            this.last = null;
+        }
+
+
+    }
+}
